2d3f8525ca5563ebd22ee1e88e9e8bf850739728
chrispanz1 <133619340+chrispanz1@users.noreply.github.com>
2023-07-24 01:19:27 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Employee from './Components /Employee';
+import employees from './models/employee';
+import './App.css';
+
+
+function App() {
+  return (
+
+    <div className="App">
+      
+      <Employee />
+    </div>
+  );
+}
+
+export default App;
